935b4ef64f179124d08fdbb2ba9232212ca726b8
kev613 <kjwagner613@gmail.com>
2025-02-04 13:29:21 -0800

    randomizer is selecting 3 and displayig for 1 sec

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const pics = [ // AI used to generate these images
-    {ID: 1, name: 'bird', link: './images/bird.png'},
-    {ID: 2, name: 'cat', link: './images/cat.png'},
-    {ID: 3, name: 'mouse', link: './images/mouse.png'},
-    {ID: 4, name: 'pig', link: './images/pig.png'},
-    {ID: 5, name: 'snake', link: './images/snake.png'},
-    {ID: 6, name: 'mongoose', link: './images/mongoose.png'},
-    {ID: 7, name: 'monkey', link: './images/monkey.png'},
-    {ID: 8, name: 'fish', link: './images/fish.png'},
-    {ID: 9, name: 'shark', link: './images/shark.png'},
-    {ID: 10, name: 'whale', link: './images/whale.png'},
-    {ID: 11, name: 'dolphin', link: '/images/dolphin.png'},
-    {ID: 12, name: 'turtle', link: './images/turtle.png'},
-];
-
+document.addEventListener('DOMContentLoaded', () => {
+    const pics = [ // AI used to generate these images
+        {ID: 1, name: 'bird', link: './images/bird.png'},
+        {ID: 2, name: 'cat', link: './images/cat.png'},
+        {ID: 3, name: 'mouse', link: './images/mouse.png'},
+        {ID: 4, name: 'pig', link: './images/pig.png'},
+        {ID: 5, name: 'snake', link: './images/snake.png'},
+        {ID: 6, name: 'mongoose', link: './images/mongoose.png'},
+        {ID: 7, name: 'monkey', link: './images/monkey.png'},
+        {ID: 8, name: 'fish', link: './images/fish.png'},
+        {ID: 9, name: 'shark', link: './images/shark.png'},
+        {ID: 10, name: 'whale', link: './images/whale.png'},
+        {ID: 11, name: 'dolphin', link: './images/dolphin.png'},
+        {ID: 12, name: 'turtle', link: './images/turtle.png'},
+    ];
 
     const firstRan = Math.floor(Math.random() * 12 + 1);
     console.log(firstRan);
@@ -23,23 +23,34 @@ const pics = [ // AI used to generate these images
     const thirdRan = Math.floor(Math.random() * 12 + 1);
     console.log(thirdRan);
 
+    const selectNameOne = pics[firstRan].name;
+    const selectLinkOne = pics[firstRan].link;
 
-const selectNameOne = pics[firstRan].name;
-const selectLinkOne = pics[firstRan].link;
+    const selectNameTwo = pics[secondRan].name;
+    const selectLinkTwo = pics[secondRan].link;
 
-const selectNameTwo = pics[secondRan].name;
-const selectLinkTwo = pics[secondRan].link;
+    const selectNameThree = pics[thirdRan].name;
+    const selectLinkThree = pics[thirdRan].link;
 
-const selectNameThree = pics[thirdRan].name;
-const selectLinkThree = pics[thirdRan].link;
+    console.log(selectNameOne, selectLinkOne);
+    console.log(selectNameTwo, selectLinkTwo);
+    console.log(selectNameThree, selectLinkThree);
 
+    function displayImages() {
+        const imageDisplay = document.getElementById('imageDisplay');
 
+        setTimeout(() => {
+            imageDisplay.src = selectLinkOne;
+        }, 0);
 
+        setTimeout(() => {
+            imageDisplay.src = selectLinkTwo;
+        }, 1000);
 
+        setTimeout(() => {
+            imageDisplay.src = selectLinkThree;
+        }, 2000);
+    }
 
-console.log(selectNameOne, selectLinkOne)
-console.log(selectNameTwo, selectLinkTwo)
-console.log(selectNameThree, selectLinkThree)
- 
-
-// console.log((Math.random() * 3)); 
+    displayImages();
+}); 
