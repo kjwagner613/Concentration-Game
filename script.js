6f39fbbcb8c6dc7efd465d7ce9e67e16acf6b880
kev613 <kjwagner613@gmail.com>
2025-02-05 14:56:37 -0800

    corrected buttons, added restart, fixed css association issues

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const startScreen = document.getElementById('start-screen');
     const gameScreen = document.getElementById('game-screen');
     const startButton = document.getElementById('start-button');
+    const replayButton = document.getElementById('replay-button');
+    const quizContainer = document.getElementById('quiz-container');
+    const resultsDiv = document.getElementById('results');
+    let selectedPictures = [];
 
     startButton.addEventListener('click', () => {
         startScreen.style.display = 'none';
         gameScreen.style.display = 'block';
-        startGame(); 
+        startGame();
     });
 
-    function startGame() { 
-        const pics = [
+    function startGame() {
+        const quizPics = [
             {ID: 1, name: 'bird', link: './images/bird.png'},
             {ID: 2, name: 'cat', link: './images/cat.png'},
             {ID: 3, name: 'mouse', link: './images/mouse.png'},
@@ -24,35 +28,35 @@ document.addEventListener('DOMContentLoaded', () => {
             {ID: 11, name: 'dolphin', link: './images/dolphin.png'},
             {ID: 12, name: 'turtle', link: './images/turtle.png'},
         ];
-        
-        const firstRan = Math.floor(Math.random() * 12 + 1);
-        const secondRan = Math.floor(Math.random() * 12 + 1);
-        const thirdRan = Math.floor(Math.random() * 12 + 1); 
 
-        const selectNameOne = pics[firstRan].name;
-        const selectLinkOne = pics[firstRan].link;
+        const firstRan = Math.floor(Math.random() * 12);
+        const secondRan = Math.floor(Math.random() * 12);
+        const thirdRan = Math.floor(Math.random() * 12);
 
-        const selectNameTwo = pics[secondRan].name;
-        const selectLinkTwo = pics[secondRan].link;
+        const selectNameOne = quizPics[firstRan].name;
+        const selectLinkOne = quizPics[firstRan].link;
 
-        const selectNameThree = pics[thirdRan].name;
-        const selectLinkThree = pics[thirdRan].link;
+        const selectNameTwo = quizPics[secondRan].name;
+        const selectLinkTwo = quizPics[secondRan].link;
+
+        const selectNameThree = quizPics[thirdRan].name;
+        const selectLinkThree = quizPics[thirdRan].link;
 
         const masterSet = [selectNameOne, selectNameTwo, selectNameThree];
 
         function displayImages() {
             const imageDisplay = document.getElementById('imageDisplay');
 
-            imageDisplay.src ="./images/black.png"; 
+            imageDisplay.src = "./images/black.png";
 
             setTimeout(() => {
                 imageDisplay.src = selectLinkOne;
             }, 1000);
-            
+
             setTimeout(() => {
-                imageDisplay.src ="./images/black.png";
+                imageDisplay.src = "./images/black.png";
             }, 1500);
-            
+
             setTimeout(() => {
                 imageDisplay.src = selectLinkTwo;
             }, 3500);
@@ -69,20 +73,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 imageDisplay.src = "./images/black.png";
             }, 7500);
 
-            setTimeout(populateQuiz, 8000); 
+            setTimeout(populateQuiz, 8000);
         }
 
         displayImages();
-        const quizContainer = document.getElementById('quiz-container');
-        const resultsDiv = document.getElementById('results');
-        let selectedPictures = [];
-        function createPictureElement(pic) { 
+
+        function createPictureElement(pic) {
             const img = document.createElement('img');
-            img.src = pic.link; 
+            img.src = pic.link;
             img.classList.add('picture');
-            img.addEventListener('click', function() {
+            img.addEventListener('click', function () {
                 if (selectedPictures.length < 3) {
-                    selectedPictures.push(pic.name); 
+                    selectedPictures.push(pic.name);
                     img.style.border = '2px solid blue';
                 } else {
                     alert("You can only select three pictures.");
@@ -91,11 +93,20 @@ document.addEventListener('DOMContentLoaded', () => {
             return img;
         }
 
-    function populateQuiz() {
-            pics.forEach(pic => {
-              quizContainer.appendChild(createPictureElement(pic));
+        function populateQuiz() {
+            quizPics.forEach(pic => {
+                quizContainer.appendChild(createPictureElement(pic));
             });
         }
+
+        const checkButton = document.createElement('button');
+        checkButton.textContent = "Check Results";
+        checkButton.className = "game-button";
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.appendChild(checkButton);
+
+        quizContainer.parentNode.insertBefore(buttonContainer, quizContainer.nextSibling);
 
         function checkResults() {
             let correctCount = 0;
@@ -104,31 +115,28 @@ document.addEventListener('DOMContentLoaded', () => {
                     correctCount++;
                 }
             });
-        
-            const percentage = Math.round((correctCount / 3) * 100); 
-        
+
+            const percentage = Math.round((correctCount / 3) * 100);
+
             let message = `You got <b>${correctCount}</b> out of **3** correct!, which is ${percentage}%`;
-        
+
             if (percentage > 60) {
-                message += " You Win!!!"; 
+                message += " You Win!!!";
             } else {
-                message += " Sorry, you lose. Do you suffer from CRS?"; 
+                message += " Sorry, you lose. Do you suffer from CRS?";
             }
-        
-            // resultsDiv.innerHTML = message; 
-            // const playAgainButton = document.createElement('button');
-            // playAgainButton.textContent = 'Play Again';
-            playAgainButton.addEventListener('click', () => {
-                location.reload();
-            });
-            resultsDiv.innerHTML += "<br>";
-            resultsDiv.appendChild(playAgainButton);
-        } 
 
-        const checkButton = document.createElement('button');
-        checkButton.textContent = "Check Results";
+            resultsDiv.innerHTML = message;
+            resultsDiv.style.display = "block";
+
+        }
+
         checkButton.addEventListener('click', checkResults);
-        resultsDiv.appendChild(checkButton);
-
-        console.log(masterSet);  
-}});
+    }
+    if (replayButton) {
+        replayButton.style.display = 'block';
+        replayButton.addEventListener('click', () => {
+            location.reload();
+        });
+    }
+});
