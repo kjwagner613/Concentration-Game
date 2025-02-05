aec3553cf797ef00e8c6d1d3e9e0fef467e15905
kev613 <kjwagner613@gmail.com>
2025-02-05 12:55:58 -0800

    Added start and play again buttons, issues but working them out

--- script.js
+++ script.js
@@ -1,132 +1,134 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const pics = [ // AI used to generate these images
-        {ID: 1, name: 'bird', link: './images/bird.png'},
-        {ID: 2, name: 'cat', link: './images/cat.png'},
-        {ID: 3, name: 'mouse', link: './images/mouse.png'},
-        {ID: 4, name: 'pig', link: './images/pig.png'},
-        {ID: 5, name: 'snake', link: './images/snake.png'},
-        {ID: 6, name: 'mongoose', link: './images/mongoose.png'},
-        {ID: 7, name: 'monkey', link: './images/monkey.png'},
-        {ID: 8, name: 'fish', link: './images/fish.png'},
-        {ID: 9, name: 'shark', link: './images/shark.png'},
-        {ID: 10, name: 'whale', link: './images/whale.png'},
-        {ID: 11, name: 'dolphin', link: './images/dolphin.png'},
-        {ID: 12, name: 'turtle', link: './images/turtle.png'},
-    ];
-    
-    const firstRan = Math.floor(Math.random() * 12 + 1);
-    console.log(firstRan);
+    const startScreen = document.getElementById('start-screen');
+    const gameScreen = document.getElementById('game-screen');
+    const startButton = document.getElementById('start-button');
 
-    const secondRan = Math.floor(Math.random() * 12 + 1);
-    console.log(secondRan);
+    startButton.addEventListener('click', () => {
+        startScreen.style.display = 'none';
+        gameScreen.style.display = 'block';
+        startGame(); 
+    });
 
-    const thirdRan = Math.floor(Math.random() * 12 + 1);
-    console.log(thirdRan);
-
-    const selectNameOne = pics[firstRan].name;
-    const selectLinkOne = pics[firstRan].link;
-
-    const selectNameTwo = pics[secondRan].name;
-    const selectLinkTwo = pics[secondRan].link;
-
-    const selectNameThree = pics[thirdRan].name;
-    const selectLinkThree = pics[thirdRan].link;
-
-    console.log(selectNameOne, selectLinkOne);
-    console.log(selectNameTwo, selectLinkTwo);
-    console.log(selectNameThree, selectLinkThree);
-
-    const masterSet = [selectNameOne, selectNameTwo, selectNameThree];
-    
-    
-    
-    function displayImages() {
-        const imageDisplay = document.getElementById('imageDisplay');
-
-        setTimeout(() => {
-            imageDisplay.src ="./images/black.png";
-        }, 0);
+    function startGame() { 
+        const pics = [
+            {ID: 1, name: 'bird', link: './images/bird.png'},
+            {ID: 2, name: 'cat', link: './images/cat.png'},
+            {ID: 3, name: 'mouse', link: './images/mouse.png'},
+            {ID: 4, name: 'pig', link: './images/pig.png'},
+            {ID: 5, name: 'snake', link: './images/snake.png'},
+            {ID: 6, name: 'mongoose', link: './images/mongoose.png'},
+            {ID: 7, name: 'monkey', link: './images/monkey.png'},
+            {ID: 8, name: 'fish', link: './images/fish.png'},
+            {ID: 9, name: 'shark', link: './images/shark.png'},
+            {ID: 10, name: 'whale', link: './images/whale.png'},
+            {ID: 11, name: 'dolphin', link: './images/dolphin.png'},
+            {ID: 12, name: 'turtle', link: './images/turtle.png'},
+        ];
         
-        setTimeout(() => {
-            imageDisplay.src = selectLinkOne;
-        }, 1000);
+        const firstRan = Math.floor(Math.random() * 12 + 1);
+        const secondRan = Math.floor(Math.random() * 12 + 1);
+        const thirdRan = Math.floor(Math.random() * 12 + 1); 
+
+        const selectNameOne = pics[firstRan].name;
+        const selectLinkOne = pics[firstRan].link;
+
+        const selectNameTwo = pics[secondRan].name;
+        const selectLinkTwo = pics[secondRan].link;
+
+        const selectNameThree = pics[thirdRan].name;
+        const selectLinkThree = pics[thirdRan].link;
+
+        const masterSet = [selectNameOne, selectNameTwo, selectNameThree];
+
+        function displayImages() {
+            const imageDisplay = document.getElementById('imageDisplay');
+
+            imageDisplay.src ="./images/black.png"; 
+
+            setTimeout(() => {
+                imageDisplay.src = selectLinkOne;
+            }, 1000);
+            
+            setTimeout(() => {
+                imageDisplay.src ="./images/black.png";
+            }, 1500);
+            
+            setTimeout(() => {
+                imageDisplay.src = selectLinkTwo;
+            }, 3500);
+
+            setTimeout(() => {
+                imageDisplay.src = "./images/black.png";
+            }, 4000);
+
+            setTimeout(() => {
+                imageDisplay.src = selectLinkThree;
+            }, 7000);
+
+            setTimeout(() => {
+                imageDisplay.src = "./images/black.png";
+            }, 7500);
+
+            setTimeout(populateQuiz, 8000); 
+        }
+
+        displayImages();
+        const quizContainer = document.getElementById('quiz-container');
+        const resultsDiv = document.getElementById('results');
+        let selectedPictures = [];
+        function createPictureElement(pic) { 
+            const img = document.createElement('img');
+            img.src = pic.link; 
+            img.classList.add('picture');
+            img.addEventListener('click', function() {
+                if (selectedPictures.length < 3) {
+                    selectedPictures.push(pic.name); 
+                    img.style.border = '2px solid blue';
+                } else {
+                    alert("You can only select three pictures.");
+                }
+            });
+            return img;
+        }
+
+    function populateQuiz() {
+            pics.forEach(pic => {
+              quizContainer.appendChild(createPictureElement(pic));
+            });
+        }
+
+        function checkResults() {
+            let correctCount = 0;
+            selectedPictures.forEach(selected => {
+                if (masterSet.includes(selected)) {
+                    correctCount++;
+                }
+            });
         
-        setTimeout(() => {
-            imageDisplay.src ="./images/black.png";
-        }, 1500);
+            const percentage = Math.round((correctCount / 3) * 100); 
         
-        setTimeout(() => {
-            imageDisplay.src = selectLinkTwo;
-        }, 3500);
+            let message = `You got <b>${correctCount}</b> out of **3** correct!, which is ${percentage}%`;
+        
+            if (percentage > 60) {
+                message += " You Win!!!"; 
+            } else {
+                message += " Sorry, you lose. Do you suffer from CRS?"; 
+            }
+        
+            // resultsDiv.innerHTML = message; 
+            // const playAgainButton = document.createElement('button');
+            // playAgainButton.textContent = 'Play Again';
+            playAgainButton.addEventListener('click', () => {
+                location.reload();
+            });
+            resultsDiv.innerHTML += "<br>";
+            resultsDiv.appendChild(playAgainButton);
+        } 
 
-        setTimeout(() => {
-            imageDisplay.src = "./images/black.png";
-        }, 4000);
+        const checkButton = document.createElement('button');
+        checkButton.textContent = "Check Results";
+        checkButton.addEventListener('click', checkResults);
+        resultsDiv.appendChild(checkButton);
 
-        setTimeout(() => {
-            imageDisplay.src = selectLinkThree;
-        }, 7000);
-
-        setTimeout(() => {
-            imageDisplay.src = "./images/black.png";
-        }, 7500)
-    }
-
-    displayImages();
-
-
-const quizContainer = document.getElementById('quiz-container');
-const resultsDiv = document.getElementById('results');
-
-// Replace these with your actual picture URLs
-const pictures = [
-  "https://example.com/image1.jpg",
-  "https://example.com/image2.jpg",
-  // ... add 12 pictures
-];
-
-// const masterSet = [selectNameOne, selectNameTwo, selectNameThree];
-
-
-
-let selectedPictures = [];
-
-// Function to create picture elements
-function createPictureElement(imageUrl) {
-  const img = document.createElement('img');
-  img.src = imageUrl;
-  img.classList.add('picture');
-  img.addEventListener('click', function() {
-    if (selectedPictures.length < 3) {
-      selectedPictures.push(imageUrl);
-      img.style.border = '2px solid blue';
-    } else {
-      alert("You can only select three pictures.");
-    }
-  });
-  return img;
-}
-
-// Populate the quiz container with pictures
-// pictures.forEach(picture => {
-//   quizContainer.appendChild(createPictureElement(picture));
-// });
-
-// Function to check results
-function checkResults() {
-  let correctCount = 0;
-  selectedPictures.forEach(selected => {
-    if (masterSet.includes(selected)) {
-      correctCount++;
-    }
-  });
-  resultsDiv.innerHTML = `You got **${correctCount}** out of **3** correct!`;
-}
-console.log(masterSet);
-
-// Add a button to check results
-// const checkButton = document.createElement('button');
-// checkButton.textContent = "Check Results";
-// checkButton.addEventListener('click', checkResults);
-// resultsDiv.appendChild(checkButton);
-}); 
+        console.log(masterSet);  
+}});
